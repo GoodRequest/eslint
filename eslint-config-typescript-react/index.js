97acbbca68fe61fe7b8985d86a0b8544fb158e65
Andrej Nemecek <andrej.nemecek@goodrequest.com>
2021-11-14 22:07:29 +0100

    Init for eslint-config-typescript-react

---
+++ eslint-config-typescript-react/index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: ["airbnb", "airbnb/hooks", "plugin:@typescript-eslint/recommended", "airbnb-typescript", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "no-unused-vars": "warn",
+    "default-case": "warn",
+    "yoda": "warn",
+    "react/jsx-curly-brace-presence": "off",
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/destructuring-assignment": "off",
+    "react/jsx-boolean-value": "off",
+    "react/no-array-index-key": "off",
+    "react-hooks/exhaustive-deps": "warn",
+    "jsx-a11y/click-events-have-key-events": "warn",
+    "jsx-a11y/no-static-element-interactions": "warn",
+    "jsx-a11y/interactive-supports-focus": "warn",
+    "jsx-a11y/label-has-associated-control": "warn",
+    "jsx-a11y/no-noninteractive-element-interactions": "warn",
+    "jsx-a11y/control-has-associated-label": "warn",
+  },
+};
